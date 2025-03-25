6ba07337c5848183efff0981297b94113a2f6a40
matiku003 <matiku003@hotmail.com>
2025-03-25 23:25:01 +0100

    Changed to miniaudio

--- src/Audio/AudioManager.cpp
+++ src/Audio/AudioManager.cpp
@@ -3,52 +3,69 @@
 
 AudioManager* AudioManager::instance = nullptr;
 
-AudioManager::AudioManager() {
-    if (soloud.init() != 0) {
-        std::cerr << "Failed to initialize SoLoud!" << std::endl;
+AudioManager::AudioManager()
+{
+    if (ma_engine_init(nullptr, &engine) != MA_SUCCESS) {
+        std::cerr << "Failed to initialize MiniAudio engine!" << std::endl;
     }
 }
 
-AudioManager::~AudioManager() {
-    soloud.deinit();
-}
+AudioManager::~AudioManager()
+{ ma_engine_uninit(&engine); }
 
-AudioManager& AudioManager::getInstance() {
+AudioManager& AudioManager::getInstance()
+{
     if (!instance) {
         instance = new AudioManager();
     }
     return *instance;
 }
 
-void AudioManager::destroyInstance() {
+void AudioManager::destroyInstance()
+{
     delete instance;
     instance = nullptr;
 }
 
-bool AudioManager::loadSound(const std::string& filename) {
-    if (currentSound.load(filename.c_str()) != 0) {
+bool AudioManager::loadSound(const std::string& filename, const std::string& id)
+{
+    ma_sound* newSound = new ma_sound;
+
+    if (ma_sound_init_from_file(&engine, filename.c_str(), 0, nullptr, nullptr, newSound) != MA_SUCCESS) {
         std::cerr << "Failed to load audio file: " << filename << std::endl;
+        delete newSound;
         return false;
     }
+
+    sounds[id] = std::shared_ptr<ma_sound>(newSound,
+                                           [](ma_sound* sound)
+                                           {
+                                               ma_sound_uninit(sound);
+                                               delete sound;
+                                           });
+
     return true;
 }
 
-void AudioManager::playSound() {
-    soloud.play(currentSound);
+void AudioManager::playSound(const std::string& id)
+{
+    auto it = sounds.find(id);
+    if (it != sounds.end()) {
+        ma_sound_start(it->second.get());
+    } else {
+        std::cerr << "Sound with ID '" << id << "' not found!" << std::endl;
+    }
 }
 
-void AudioManager::setVolume(float volume) {
-    soloud.setGlobalVolume(volume);
-}
+void AudioManager::setVolume(float volume)
+{ ma_engine_set_volume(&engine, volume); }
 
-void AudioManager::pauseSound() {
-    soloud.setPauseAll(true);
-}
-
-void AudioManager::resumeSound() {
-    soloud.setPauseAll(false);
-}
-
-void AudioManager::stopSound() {
-    soloud.stopAll();
+void AudioManager::stopSound(const std::string& id)
+{
+    auto it = sounds.find(id);
+    if (it != sounds.end()) {
+        ma_sound_stop(it->second.get());
+    } else {
+        std::cerr << "Sound with ID '" << id << "' not found!" << std::endl;
+    }
 }
