818ce564f307d22162608358783f602311f67861
kacper119p <kacper119p@gmail.com>
2025-03-30 06:37:33 +0200

    Update AssertionsUtility.h

--- src/Utility/AssertionsUtility.h
+++ src/Utility/AssertionsUtility.h
@@ -1,13 +1,13 @@
 #pragma once
 #if DEBUG
-#define  CHECK_MESSAGE(__EXPRESSION__,__MESSAGE__)\
+#define CHECK_MESSAGE(__EXPRESSION__,__MESSAGE__)\
 if(!!(!(__EXPRESSION__)))\
 {\
     fprintf(stderr, "%s %d: Assertion failed on expression: %s: %s", __FILE__, __LINE__, #__EXPRESSION__,  __MESSAGE__);\
     abort();\
 }
 #else
-#define  CHECK_M(__EXPRESSION__, __MESSAGE__)
+#define CHECK_MESSAGE(__EXPRESSION__, __MESSAGE__)
 #endif
 
 #if DEBUG
