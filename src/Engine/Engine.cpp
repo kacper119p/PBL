0146c9471b39a0538855ef4a590deed14796d2ad
matiku003 <matiku003@hotmail.com>
2025-06-09 01:14:27 +0200

    Path fix

--- src/Engine/Engine.cpp
+++ src/Engine/Engine.cpp
@@ -193,7 +193,7 @@ namespace Engine
 #endif
 
         // Create window with graphics context
-        Window = glfwCreateWindow(mode->width, mode->height, "Tide Engine", nullptr, nullptr);
+        Window = glfwCreateWindow(mode->width, mode->height, "Tide Engine", monitor, nullptr);
         if (!Window)
         {
             spdlog::error("Failed to create GLFW Window!");
