9943af41812be2573825a690ee943d537af5b92b
kacper119p <kacper119p@gmail.com>
2025-06-20 13:24:19 +0200

    Reverted unwanted changes

--- src/Scene/SceneBuilder.cpp
+++ src/Scene/SceneBuilder.cpp
@@ -21,7 +21,7 @@ namespace Scene
         ZoneScoped;
 
         Scene = new class Engine::Scene();
-        Engine::SceneManager::LoadScene("./res/scenes/Gameplay.lvl", Scene);
+        Engine::SceneManager::LoadScene("./res/scenes/SampleScene.lvl", Scene);
         // TODO: remove when no longer needed
 
 
