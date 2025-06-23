fb918c435b9f20e2e85dbba7ddb21ea13acfdb55
kacper119p <kacper119p@gmail.com>
2025-06-23 18:03:22 +0200

    MopEmitter class placeholder

---
+++ src/Engine/Components/VFX/MopEmitter.cpp
@@ -0,0 +1,142 @@
+#include "MopEmitter.h"
+#include "Engine/EngineObjects/Entity.h"
+#include "Engine/EngineObjects/LightManager.h"
+#include <algorithm>
+#include <imgui.h>
+#include <GLFW/glfw3.h>
+
+#include "Engine/EngineObjects/RenderingManager.h"
+#include "Engine/Rendering/SpriteQuad.h"
+#include "Serialization/SerializationUtility.h"
+#include "Shaders/ShaderManager.h"
+
+Engine::MopEmitter::MopEmitter() :
+    ParticleEmitter(Materials::MaterialManager::GetMaterial("res/materials/VFX/Flame.mat"),
+                    Shaders::ShaderManager::GetComputeShader("res/shaders/VFX/Fire/Flame/FlameSpawn.comp"),
+                    Shaders::ShaderManager::GetComputeShader("res/shaders/VFX/Fire/Flame/FlameUpdate.comp")),
+    ParticlesToSpawnProperty(SpawnShader.GetUniformLocation("ParticlesToSpawn")),
+    DeltaTimeProperty(UpdateShader.GetUniformLocation("DeltaTime")),
+    RandomProperty(SpawnShader.GetUniformLocation("Random")),
+    ViewMatrixLocation(Material->GetMainPass().GetUniformLocation("ViewMatrix")),
+    ProjectionMatrixLocation(Material->GetMainPass().GetUniformLocation("ProjectionMatrix")),
+    ObjectToWorldMatrixLocation(Material->GetMainPass().GetUniformLocation("ObjectToWorldMatrix"))
+{
+}
+
+Engine::MopEmitter::~MopEmitter()
+{
+    glDeleteBuffers(1, &ParticlesBuffer);
+    glDeleteBuffers(1, &FreelistBuffer);
+    glDeleteBuffers(1, &SortedIndicesBuffer);
+}
+
+void Engine::MopEmitter::Render(const Engine::CameraRenderData& RenderData)
+{
+    Shaders::Shader::SetUniform(ViewMatrixLocation, RenderData.ViewMatrix);
+    Shaders::Shader::SetUniform(ProjectionMatrixLocation, RenderData.ProjectionMatrix);
+    Shaders::Shader::SetUniform(ObjectToWorldMatrixLocation, GetOwner()->GetTransform()->GetLocalToWorldMatrix());
+
+    SetupMatrices(RenderData, Material->GetMainPass());
+    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, ParticlesBuffer);
+    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 2, SortedIndicesBuffer);
+    SpriteQuad::DrawInstanced(MaxParticleCount);
+}
+
+void Engine::MopEmitter::Start()
+{
+    ParticleEmitter::Start();
+
+    glGenBuffers(1, &ParticlesBuffer);
+    glGenBuffers(1, &FreelistBuffer);
+    glGenBuffers(1, &SortedIndicesBuffer);
+    glBindBuffer(GL_SHADER_STORAGE_BUFFER, ParticlesBuffer);
+
+    const Particle* particles = new Particle[MaxParticleCount]{};
+    glBufferData(GL_SHADER_STORAGE_BUFFER, MaxParticleCount * sizeof(Particle), particles, GL_DYNAMIC_DRAW);
+    delete[] particles;
+
+    glBindBuffer(GL_SHADER_STORAGE_BUFFER, FreelistBuffer);
+    int* freeList = new int[MaxParticleCount + 1];
+    freeList[0] = MaxParticleCount;
+    for (int i = 1; i <= MaxParticleCount; ++i)
+    {
+        freeList[i] = i - 1;
+
+    }
+    glBufferData(GL_SHADER_STORAGE_BUFFER, (MaxParticleCount + 1) * sizeof(int), freeList, GL_DYNAMIC_DRAW);
+    delete[] freeList;
+
+    glBindBuffer(GL_SHADER_STORAGE_BUFFER, SortedIndicesBuffer);
+    const int* sortedIndices = new int[MaxParticleCount];
+    glBufferData(GL_SHADER_STORAGE_BUFFER, (MaxParticleCount) * sizeof(int), sortedIndices, GL_DYNAMIC_DRAW);
+    delete[] sortedIndices;
+
+    glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
+}
+
+void Engine::MopEmitter::DispatchSpawnShaders(const float DeltaTime)
+{
+    Timer += DeltaTime;
+
+    const int particlesToSpawn = std::min(static_cast<int>(Timer * SpawnRate), MaxParticleCount);
+    Timer -= static_cast<float>(particlesToSpawn) / SpawnRate;
+
+    if (particlesToSpawn > 0)
+    {
+        SpawnShader.Use();
+
+        glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, ParticlesBuffer);
+        glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, FreelistBuffer);
+
+        constexpr int workGroupSize = 64;
+        const int workGroupsCount = (particlesToSpawn + workGroupSize - 1) / workGroupSize;
+
+        Shaders::ComputeShader::SetUniform(ParticlesToSpawnProperty, particlesToSpawn);
+        float time = static_cast<float>(glfwGetTime());
+        Shaders::ComputeShader::SetUniform(RandomProperty, *reinterpret_cast<unsigned int*>(&time));
+
+        Shaders::ComputeShader::Dispatch(glm::ivec3(workGroupsCount, 1, 1));
+        glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
+    }
+}
+
+void Engine::MopEmitter::DispatchUpdateShaders(const float DeltaTime)
+{
+    UpdateShader.Use();
+    Shaders::ComputeShader::SetUniform(DeltaTimeProperty, DeltaTime);
+    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, ParticlesBuffer);
+    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, FreelistBuffer);
+    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 2, SortedIndicesBuffer);
+
+    constexpr int workGroupSize = 64;
+    constexpr int workGroupsCount = (MaxParticleCount + workGroupSize - 1) / workGroupSize;
+    Shaders::ComputeShader::Dispatch(glm::ivec3(workGroupsCount, 1, 1));
+}
+
+#if EDITOR
+void Engine::MopEmitter::DrawImGui()
+{
+    if (ImGui::CollapsingHeader("Fire Particle Emitter"))
+    {
+    }
+}
+#endif
+rapidjson::Value Engine::MopEmitter::Serialize(rapidjson::Document::AllocatorType& Allocator) const
+{
+    START_COMPONENT_SERIALIZATION
+    END_COMPONENT_SERIALIZATION
+}
+
+void Engine::MopEmitter::DeserializeValuePass(const rapidjson::Value& Object,
+                                                       Serialization::ReferenceTable& ReferenceMap)
+{
+    START_COMPONENT_DESERIALIZATION_VALUE_PASS
+    END_COMPONENT_DESERIALIZATION_VALUE_PASS
+}
+
+void Engine::MopEmitter::DeserializeReferencesPass(const rapidjson::Value& Object,
+                                                            Serialization::ReferenceTable& ReferenceMap)
+{
+    START_COMPONENT_DESERIALIZATION_REFERENCES_PASS
+    END_COMPONENT_DESERIALIZATION_REFERENCES_PASS
+}
---
+++ src/Engine/Components/VFX/MopEmitter.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "ParticleEmitter.h"
+#include "Shaders/ComputeShader.h"
+#include "Models/Model.h"
+
+namespace Engine
+{
+    class MopEmitter final : public ParticleEmitter
+    {
+    private:
+        struct alignas(16) Particle
+        {
+            glm::vec3 Position = glm::vec3(0.0f);
+            float Scale = 0.0f;
+            glm::vec3 Velocity = glm::vec3(0.0f);
+            float Life = -1.0f;
+        };
+
+        static_assert(sizeof(Particle) == 32, "Size mismatch with GLSL buffer");
+
+    private:
+        static constexpr float MaxLifetime = 64.0f / 24.0f;
+        static constexpr int32_t MaxParticleCount = 64;
+        static constexpr float SpawnRate = MaxParticleCount / MaxLifetime;
+
+        float Timer = 0.0f;
+
+        uint32_t ParticlesBuffer = 0;
+        uint32_t FreelistBuffer = 0;
+        uint32_t SortedIndicesBuffer = 0;
+
+        int32_t ParticlesToSpawnProperty = 0;
+        int32_t DeltaTimeProperty = 0;
+        int32_t RandomProperty = 0;
+
+        int32_t ViewMatrixLocation;
+        int32_t ProjectionMatrixLocation;
+        int32_t ObjectToWorldMatrixLocation;
+
+    public:
+        MopEmitter();
+
+    public:
+        ~MopEmitter() override;
+
+    public:
+        void Start() override;
+
+        void DispatchSpawnShaders(float DeltaTime) override;
+
+        void DispatchUpdateShaders(float DeltaTime) override;
+
+        void Render(const CameraRenderData& RenderData) override;
+
+#if EDITOR
+        void DrawImGui() override;
+#endif
+        SERIALIZATION_EXPORT_CLASS(MopEmitter)
+    };
+}
