5eb59aca967f013210e280741a5adf44d8b41194
kacper119p <kacper119p@gmail.com>
2025-06-26 01:06:34 +0200

    Finished Dialogue widget implementation

--- src/Engine/UI/DialogueWidget.cpp
+++ src/Engine/UI/DialogueWidget.cpp
@@ -3,6 +3,7 @@
 #include "Engine/Textures/TextureManager.h"
 #include "Materials/MaterialManager.h"
 #include "Materials/Ui/BasicImageMaterial.h"
+#include "Math/Easings.h"
 
 namespace Engine::Ui
 {
@@ -15,14 +16,14 @@ namespace Engine::Ui
         Image->SetTexture(TextureManager::GetTexture("res/textures/Default.dds"));
         Image->SetMaterial(imageMaterial);
 
-        Image->GetRect().SetPositionPixels(glm::vec3(0, -468 + 25, 0.5));
+        Image->GetRect().SetPositionPixels(glm::vec3(HiddenPositionImage));
         Image->GetRect().SetSizePixels(glm::vec2(1470, 144 + 50));
 
         Text = new class Text();
         Text->SetFont("EagleLakeRegular");
         Text->GetRect().SetParent(&GetRect());
 
-        Text->GetRect().SetPositionPixels(glm::vec3(-700, -308, 0.5));
+        Text->GetRect().SetPositionPixels(glm::vec3(HiddenPositionImage));
         Text->GetRect().SetSizePixels(glm::vec2(1400 * 2, 70));
     }
 
@@ -35,18 +36,52 @@ namespace Engine::Ui
 
     void DialogueWidget::Update(const float DeltaTime)
     {
-        if (!FinishedAnimation)
+        Timer += DeltaTime;
+
+        switch (CurrentAnimation)
         {
-            Timer += DeltaTime;
-            const int32_t characterCount = static_cast<int32_t>(Timer / TimePerCharacter);
-            const std::string str = CurrentText.substr(0, characterCount);
-            if (Text->GetText() != str)
+            case AnimationType::TextEnter:
             {
-                Text->SetText(str);
-                if (characterCount == CurrentText.length())
+                if (!FinishedAnimation)
+                {
+
+                    const int32_t characterCount = static_cast<int32_t>(Timer / TimePerCharacter);
+                    const std::string str = CurrentText.substr(0, characterCount);
+                    if (Text->GetText() != str)
+                    {
+                        Text->SetText(str);
+                        if (characterCount >= CurrentText.length())
+                        {
+                            FinishedAnimation = true;
+                        }
+                    }
+                }
+                break;
+            }
+            case AnimationType::Enter:
+            {
+                float time = Timer / TransitionTime;
+                time = Math::EaseOutBack(time);
+                Text->GetRect().SetPositionPixels(glm::mix(HiddenPositionText, ShownPositionText, time));
+                Image->GetRect().SetPositionPixels(glm::mix(HiddenPositionImage, ShownPositionImage, time));
+                if (Timer >= TransitionTime)
                 {
                     FinishedAnimation = true;
                 }
+                break;
+            }
+            case AnimationType::Exit:
+            {
+                float time = 1.0f - Timer / TransitionTime;
+                time = Math::EaseOutBack(time);
+                Text->GetRect().SetPositionPixels(glm::mix(HiddenPositionText, ShownPositionText, time));
+                Image->GetRect().SetPositionPixels(glm::mix(HiddenPositionImage, ShownPositionImage, time));
+                if (Timer >= TransitionTime)
+                {
+                    Text->SetText(std::string());
+                    FinishedAnimation = true;
+                }
+                break;
             }
         }
     }
